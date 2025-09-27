1ec1f70b74bac5cb3508ddaea1a8a3e8625033a7
jonakack <jonathan.kacker@gmail.com>
2025-09-27 10:34:41 +0200

    Safety improvements in saveData function

--- include/cache.h
+++ include/cache.h
@@ -1,8 +1,8 @@
 #ifndef CACHE_H
 #define CACHE_H
 
-#define OK 0
 #define ERROR 1
+#define OK 0
 #define UP_TO_DATE 0
 #define OUT_OF_DATE -1
 #define DOES_NOT_EXIST -2
--- src/cache.c
+++ src/cache.c
@@ -20,14 +20,12 @@
 
 int saveData(char *httpData, int i)
 {
-    FILE *fptr;
+    FILE *fptr = NULL;
     char filename[100];
+    int result = OK; // Initializing return value
 
     cJSON *json_obj = cJSON_Parse(httpData);     // Parse data
     char *httpData_json = cJSON_Print(json_obj); // Put parsed JSON data into a string
-
-    // free(httpData_json);
-    // cJSON_Delete(json_obj);
 
     // Create folder called "data"
     if (MKDIR("data") != OK)
@@ -39,60 +37,83 @@ int saveData(char *httpData, int i)
         else
         {
             perror("Failed to create directory");
-            return ERROR;
+            result = ERROR;
+            goto cleanup;
         }
     }
 
     // Set file name
     sprintf(filename, "data/%s_%.4f_%.4f.json", cities[i - 1].name, cities[i - 1].latitude, cities[i - 1].longitude);
 
-    if (check_data_age(filename) == OUT_OF_DATE || DOES_NOT_EXIST)
+    int fileStatusResult = check_data_age(filename);
+    if (fileStatusResult == OUT_OF_DATE || fileStatusResult == DOES_NOT_EXIST)
     {
         // Create file in data folder
         if ((fptr = fopen(filename, "w")) == NULL)
         {
             perror("Failed to open file");
-            return ERROR;
+            result = ERROR;
+            goto cleanup;
         }
         else
         {
             if (fprintf(fptr, "%s", httpData_json) < OK)
             {
                 perror("Failed to write to file");
-                return ERROR;
+                result = ERROR;
+                goto cleanup;
             }
             if (fclose(fptr) != OK)
             {
                 perror("Failed to close file");
-                return ERROR;
+                result = ERROR;
+                goto cleanup;
             }
+            fptr = NULL;
         }
-        return OUT_OF_DATE; // Made a new file
+        result = OUT_OF_DATE; // No new file needed
+        goto cleanup;
     }
-    return OK; // No new file needed
+
+cleanup:
+    if (fptr != NULL)
+    {
+        fclose(fptr);
+    }
+    free(httpData_json);
+    cJSON_Delete(json_obj);
+    return result;
 }
 
 int check_data_age(const char *filename)
 {
     struct stat fileStatus;
 
-    if (stat(filename, &fileStatus) == OK)
+    if (stat(filename, &fileStatus) != OK)
     {
-        time_t mod_time = fileStatus.st_mtime;
-        time_t current_time = time(NULL);
-
-        if ((current_time - mod_time) > 900) // Checks if file is older than 15 minutes
+        if (errno == ENOENT) // errno checks if error is because file doesn't exist
         {
-            printf("File is older than 15 minutes, creating new one... \n");
-            return OUT_OF_DATE;
+            printf("File does not exist, creating new one... \n");
+            return DOES_NOT_EXIST;
         }
         else
         {
-            printf("File is up to date. No new file needed. \n");
-            return UP_TO_DATE;
+            perror("Failed to get file status");
+            return ERROR;
         }
     }
+
+    time_t mod_time = fileStatus.st_mtime;
+    time_t current_time = time(NULL);
+
+    if ((current_time - mod_time) > 900) // Checks if file is older than 15 minutes
+    {
+        printf("File is older than 15 minutes, creating new one... \n");
+        return OUT_OF_DATE;
+    }
     else
-        printf("File does not exist, creating new one... \n");
-    return DOES_NOT_EXIST; // Create a new one
+    {
+        printf("File is up to date. No new file needed. \n");
+        return UP_TO_DATE;
+    }
 }
