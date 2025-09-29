101df9de2178e85a93ac9a3a939a5a4f2d14fe95
ochowun9gmailcom <ochowun9@gmail.com>
2025-09-29 13:17:59 +0200

    Lägg till väder code funktion

--- src/meteo.c
+++ src/meteo.c
@@ -11,3 +11,42 @@ void makeURL(int cityIndex, char *url) {
             cities[cityIndex-1].longitude);
     while (getchar() != '\n'); // clear the input buffer so \n doesn't follow into the next user input
 }
+
+void meteoWeatherCodes(int code, char *desc) {
+    static const char *weather[] = {
+        [0] = "Clear sky ☀️",
+        [1] = "Mainly clear 🌤️",
+        [2] = "Partly cloudy ⛅",
+        [3] = "Overcast ☁️",
+        [45] = "Fog 🌫️",
+        [48] = "Depositing rime fog 🌫️",
+        [51] = "Light drizzle 🌦️",
+        [53] = "Moderate drizzle 🌦️",
+        [55] = "Dense drizzle 🌧️",
+        [56] = "Light freezing drizzle 🌧️",
+        [57] = "Dense freezing drizzle 🌧️",
+        [61] = "Slight rain 🌧️",
+        [63] = "Moderate rain 🌧️",
+        [65] = "Heavy rain ⛈️",
+        [66] = "Light freezing rain 🌧️",
+        [67] = "Heavy freezing rain 🌧️",
+        [71] = "Slight snow fall ❄️",
+        [73] = "Moderate snow fall ❄️",
+        [75] = "Heavy snow fall ❄️",
+        [77] = "Snow grains ❄️",
+        [80] = "Slight rain showers 🌦️",
+        [81] = "Moderate rain showers 🌧️",
+        [82] = "Violent rain showers ⛈️",
+        [85] = "Slight snow showers 🌨️",
+        [86] = "Heavy snow showers 🌨️",
+        [95] = "Thunderstorm ⛈️",
+        [96] = "Thunderstorm with slight hail ⛈️",
+        [99] = "Thunderstorm with heavy hail ⛈️"
+    };
+
+    if (code >= 0 && code < 100 && weather[code] != NULL) {
+        sprintf(desc, "%s", weather[code]);
+    } else {
+        sprintf(desc, "Unknown weather code (%d)", code);
+    }
+}
